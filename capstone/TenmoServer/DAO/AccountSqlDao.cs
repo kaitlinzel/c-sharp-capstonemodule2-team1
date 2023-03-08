f96ac08874118d7e692f2e76df3ab2037504a332
Ben Mullaly <benmullaly14gmail.com>
2023-03-08 14:47:06 -0500

    working on Account controller and account DAOs

---
+++ capstone/TenmoServer/DAO/AccountSqlDao.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using TenmoServer.Models;
+namespace TenmoServer.DAO
+{
+    public class AccountSqlDao : IAccountDao
+    {
+        private readonly string ConnectionString;
+
+        public AccountSqlDao(string dbConnectionString)
+        {
+            ConnectionString = dbConnectionString;
+        }
+       
+        public Account GetAccountByAccountId(int accountId)
+        {
+            return new Account();
+        }
+        public IList<Account> GetAccountsByUserId(int userId)
+        {
+            return new List<Account>();
+        }
+        public Account AddAccountToUser(int userId, Account account)
+        {
+            return new Account();
+        }
+
+
+    }
+}
---
+++ capstone/TenmoServer/DAO/IAccountDao.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using TenmoServer.Models;
+namespace TenmoServer.DAO
+{
+    public interface IAccountDao
+    {
+        Account GetAccountByAccountId(int accountId);
+        IList<Account> GetAccountsByUserId(int userId);
+        Account AddAccountToUser(int userId, Account account);
+    }
+}
