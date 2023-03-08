b206a691c3763d4b931d0c203328fc3dd565f968
Kaitlin Zelmanovich <kaitlinmgarman@gmail.com>
2023-03-08 12:14:01 -0500

    KZ added controllers 12:13 pm 3/8/23

---
+++ capstone/TenmoServer/Controllers/BalanceController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TenmoServer.DAO;
+
+namespace TenmoServer.Controllers
+{
+    [Route("[controller")]
+    [ApiController]
+    public class BalanceController : Controller
+    {
+        private IUserDao userDao; 
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles = "user")] // we want to authorize this to only showing balance for userId when user IDs match. not sure if "user" is correct
+        [HttpGet("{id}")]
+        public ActionResult<Balance> GetBalance(int id)
+        {
+            Balance balance = userDao.Get(id); 
+
+            if(balance != null)
+            {
+                return balance; 
+            }
+            else
+            {
+                return NotFound(); 
+            }
+        }
+    }
+}
---
+++ capstone/TenmoServer/Controllers/TransferController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization; 
+
+namespace TenmoServer.Controllers
+{
+    [Route("[controller")]
+    [ApiController]
+    public class TransferController : Controller
+    {
+        private ITransferDao transferDao; 
+
+        public TransferController(ITransferDao transferDao)
+        {
+            this.transferDao = transferDao; 
+
+        }
+    
+     
+    
+    }
+
+}
